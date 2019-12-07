0416bfc79af752ecbad3c20e228e1c2ce3b3ae11
azraeihdi <azraeihdi@gmail.com>
2019-12-08 01:36:36 +0800

    Updated Map_Control.java FXML items

--- Diamond Hunter/src/com/neet/DiamondHunter/MapViewer/Map_Controls.java
+++ Diamond Hunter/src/com/neet/DiamondHunter/MapViewer/Map_Controls.java
@@ -171,6 +171,159 @@ public class Map_Controls{
             }
         }
     });
-}
+	}
+	
+	
+	
+	
+	
+	
+	@FXML
+    public void back() throws Exception{
+        Scene scene = back.getScene();
+        Stage currentscene = (Stage)scene.getWindow();
+        currentscene.hide();
+    }
     
+    public void loadItems(String s) {
+        Image setTile = new Image(s);
+        itemss = new Image[2];
+        for (int col = 0; col < 2; col++) {
+            itemss[col] = new WritableImage(
+                    setTile.getPixelReader(),
+                    col * tileSize,
+                    16,
+
+                    tileSize,
+                    tileSize);
+        }
+    }
+    public void loadTiles(String s) {
+
+        try {
+
+            Image setTile=new Image(s);
+            numTilesAcross = (int) setTile.getWidth() / tileSize;
+            tiles = new Image[2][numTilesAcross];
+
+            for(int col = 0; col < numTilesAcross; col++) {
+                tiles[0][col] = new WritableImage(
+                        setTile.getPixelReader(),
+                        col * tileSize,
+                        0,
+                        tileSize,
+                        tileSize);
+                tiles[1][col] = new WritableImage(
+                        setTile.getPixelReader(),
+                        col * tileSize,
+                        tileSize,
+                        tileSize,
+                        tileSize);
+            }
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void loadMap(String s) {
+
+        try {
+
+            InputStream in = getClass().getResourceAsStream(s);
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(in)
+            );
+
+            numCols = Integer.parseInt(br.readLine());
+            numRows = Integer.parseInt(br.readLine());
+            map = new int[numRows][numCols];
+
+            String delims = "\\s+";
+            for(int row = 0; row < numRows; row++) {
+                String line = br.readLine();
+                String[] tokens = line.split(delims);
+                for(int col = 0; col < numCols; col++) {
+                    map[row][col] = Integer.parseInt(tokens[col]);
+                }
+            }
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void draw(GraphicsContext g) {
+
+        for(int row = 0; row < 40; row++) {
+
+            if(row >= numRows) break;
+
+            for(int col = 0; col < 40; col++) {
+
+                if(col >= numCols) break;
+                if(map[row][col] == 0) continue;
+
+                int rc = map[row][col];
+                int r = rc / numTilesAcross;
+                int c = rc % numTilesAcross;
+
+                g.drawImage(
+                        tiles[r][c],
+                        col * tileSize,
+                        row * tileSize
+                );
+
+            }
+
+        }
+
+    }
+    
+    
+    private int axeX=26,axeY=37,boatX=12,boatY=4;
+    public static int save_axeX=416,save_axeY=592,save_boatX=192,save_boatY=64;
+    int select=0;
+    boolean first_boat=true,first_axe=true;
+
+    private int[][] map;
+    private int tileSize=16;
+    private int numRows;
+    private int numCols;
+
+    private Image[][] tiles;
+    private Image[] itemss;
+    public Image image;
+    private int numTilesAcross;
+    
+    
+
+    @FXML
+    public Canvas canvas;
+
+    @FXML
+    private TextField axex;
+
+    @FXML
+    private Button save;
+    
+    @FXML
+    private Button back;
+
+    @FXML
+    private TextField axey;
+
+    @FXML
+    private TextField boatx;
+
+    @FXML
+    private TextField boaty;
+
+    @FXML
+    private Button boat;
+
+    @FXML
+    private Button axe;
 }
