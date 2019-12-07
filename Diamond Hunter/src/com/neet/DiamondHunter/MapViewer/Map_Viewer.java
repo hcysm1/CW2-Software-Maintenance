79d4bbd3558d18724dc800eddbacfbcfee05cc2f
Shrabana Biswas Shruti <hcyss2@nottingham.edu.my>
2019-12-07 19:31:24 +0800

    Updated Map_Viewer

--- Diamond Hunter/src/com/neet/DiamondHunter/MapViewer/Map_Viewer.java
+++ Diamond Hunter/src/com/neet/DiamondHunter/MapViewer/Map_Viewer.java
@@ -1,11 +1,17 @@
 package com.neet.DiamondHunter.MapViewer;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
-public class Map_Viewer{
+
+public class Map_Viewer  {
 
     public static void main(String[] args) {
-        
+    	Application.launch();
     }
 
   
