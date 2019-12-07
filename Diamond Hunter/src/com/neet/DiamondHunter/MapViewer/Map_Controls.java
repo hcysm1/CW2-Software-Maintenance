b0c672507a91a1d36ba42410baef1ea21e62d43d
Widanage Vinuri De Silva <hfywd1@nottingham.edu.my>
2019-12-08 04:15:06 +0800

    Adding new files to remote repository

---
+++ Diamond Hunter/src/com/neet/DiamondHunter/MapViewer/Map_Controls.java
@@ -0,0 +1,33 @@
+package com.neet.DiamondHunter.MapViewer;
+
+
+
+public class Map_Controls implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+     
+
+        Alert saved = new Alert(Alert.AlertType.INFORMATION);
+        saved.setTitle("Axe and Boat");
+        saved.setHeaderText("Changes has been saved.");
+
+        GraphicsContext g = canvas.getGraphicsContext2D();
+        loadTiles("/Tilesets/testtileset.gif");
+        loadItems("/Sprites/items.gif");
+        loadMap("/Maps/testmap.map");
+
+        // Draw Initial Map and Item Position
+        draw(g);
+        g.drawImage(
+                itemss[0],
+                save_boatY,save_boatX
+        );
+        g.drawImage(
+                itemss[1],
+                save_axeY,save_axeX
+        );
+
+    
+}
