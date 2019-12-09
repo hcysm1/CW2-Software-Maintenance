3dfbd01a89e0ed8e72e7f40fc8da7d4865aa8fef
hcyss2 <57673725+hcyss2@users.noreply.github.com>
2019-12-09 21:47:43 +0800

    Formatted code

--- Diamond Hunter/src/com/neet/DiamondHunter/Main/MapmenuController.java
+++ Diamond Hunter/src/com/neet/DiamondHunter/Main/MapmenuController.java
@@ -1,53 +1,53 @@
-package com.neet.DiamondHunter.Main;
+	package com.neet.DiamondHunter.Main;
 
-import javax.swing.*;
-
-
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+	import javax.swing.*;
 
 
-public class MapmenuController {
-	
-	Stage primaryStage= new Stage();
-	private static JFrame window;
 
-	
-	// Event Listener on Button.onAction
-	@FXML
-	public void rungame(ActionEvent event) {
-		System.out.println("Run game clicked");
-		window = new JFrame("Diamond Hunter");	
-		
-		window.add(new GamePanel());
-		
-		window.setResizable(false);
-		window.pack();
-		
-		window.setLocationRelativeTo(null);
-		window.setVisible(true);
-		window.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+	import javafx.event.ActionEvent;
+	import javafx.fxml.FXML;
+	import javafx.fxml.FXMLLoader;
+	import javafx.scene.Parent;
+	import javafx.scene.Scene;
+	import javafx.stage.Stage;
+
+
+	public class MapmenuController {
+
+		Stage primaryStage= new Stage();
+		private static JFrame window;
+
+
+		// Event Listener on Button.onAction
+		@FXML
+		public void rungame(ActionEvent event) {
+			System.out.println("Run game clicked");
+			window = new JFrame("Diamond Hunter");	
+
+			window.add(new GamePanel());
+
+			window.setResizable(false);
+			window.pack();
+
+			window.setLocationRelativeTo(null);
+			window.setVisible(true);
+			window.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+
+		}
+
+		public static JFrame getWindow(){
+			return window;
+		}
+
+		@FXML
+		public void mapviewer() throws Exception{
+
+			Parent root = FXMLLoader.load(getClass().getResource("../MapViewer/Map_Editor.fxml"));
+		primaryStage.setTitle("Edit Map");        
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
 
 	}
-	
-	public static JFrame getWindow(){
-		return window;
 	}
-	
-	@FXML
-	public void mapviewer() throws Exception{
-		
-		Parent root = FXMLLoader.load(getClass().getResource("../MapViewer/Map_Editor.fxml"));
-        primaryStage.setTitle("Edit Map");        
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.show();
-	
-}
-}
